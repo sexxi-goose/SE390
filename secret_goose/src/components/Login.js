566a15bc80a7379ef0eb1815a2fbe64bc3dea454
Aman Arora <me@aman-arora.com>
2019-09-23 17:57:46 -0400

    Frontend websocket connection and handshake

--- secret_goose/src/components/Login.js
+++ secret_goose/src/components/Login.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './../Style/Login.css';
 import { Redirect } from 'react-router-dom'
+import socketIOClient from 'socket.io-client'
+
+const REQUEST_USERID_ROOMID = "SendUseridRoomid"
+const RESPONSE_USERID_ROOMID = "UseridRoomid"
+const NEW_USER_JOINED_ROOM = "NewUserJoinedRoom"
+const ENDPOINT = ""
 
 export class LoginForm extends React.Component {
   constructor(props) {
@@ -10,6 +16,8 @@ export class LoginForm extends React.Component {
       username: "",
       redirectToGame: false
     };
+
+    this.socketConnection = null
   }
 
   handleChange = (event) => {
@@ -26,6 +34,20 @@ export class LoginForm extends React.Component {
     this.setState({redirectToGame:true})
   }
 
+  setupWebsocket = () => {
+    this.socketConnection = socketIOClient(ENDPOINT);
+    this.socketConnection.on(NEW_USER_JOINED_ROOM, data => {
+      console.log(data["username"] + " joined the room");
+    });
+
+    this.socketConnection.on(REQUEST_USERID_ROOMID, (data) => {
+      this.socketConnection.emit(RESPONSE_USERID_ROOMID, {
+        room_id: this.state.roomId,
+        user_id: this.state.username
+      });
+    });
+  }
+
   handleJoinRoom = (event) => {
     event.preventDefault();
 
@@ -39,6 +61,8 @@ export class LoginForm extends React.Component {
           username: this.state.username
         }),
     });
+
+    this.setupWebsocket();
     this.routeToGame();
   }
 
@@ -55,6 +79,7 @@ export class LoginForm extends React.Component {
         }),
     });
 
+    this.setupWebsocket();
     this.routeToGame();
   }
 
