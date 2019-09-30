b68430c56dfad9303aac73c90589aaf5b79d0bf2
Aman Arora <aman23091998@gmail.com>
2019-09-29 20:42:26 -0400

    CORS

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import socketio
 
 from game_sessions import games
 from flask import Flask
+from flask_cors import CORS
 
 
 app = Flask(
@@ -16,6 +17,8 @@ app.debug = True
 app.register_blueprint(homepage.home_endpoint)
 app.register_blueprint(game_api.game_endpoint)
 app.wsgi_app = socketio.WSGIApp(server.server, app.wsgi_app)
+
+CORS(app)
 
 def main():
     games.set_websocket_server(server.server)
